da2a9228e9c06b2d0052ce8aee5969360547a202
Zeeshan Ali <zeeshanali.4611@gmail.com>
2024-10-01 15:09:10 +0000

    nested dictionaries

--- nestedDictionary.py
+++ nestedDictionary.py
@@ -1,14 +1,17 @@
 mydict = {
-    {
+    "dict1":{
         "name": "Zeeshan",
         "email": "zeeshan.it58@gmail.com",
         "age" : 28
     },
-    {
+    "dict2":{
         "profession": "developer",
         "city": "Lahore",
         "designation": "SSE"
     }
 }
 
+for x, obj in mydict.items():
+    for y in obj:
+        print(obj[y])
 #now we'll access this with loop
